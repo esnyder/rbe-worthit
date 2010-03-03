27ea17a7192c390ce23a06be361aec1e7bf7aca0
Emile Snyder <emile.snyder@gmail.com>
2010-03-03 14:28:23 -0800

    Fix invalid ISBN issue: get a list of bad ones at the top of the output.
    Fix the no-CD/DVD/etc.. Now handles them.

--- isbnsearch.py
+++ isbnsearch.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 #
 
-import os, sys, re, lxml, cgi, cgitb, unicodedata
+import os, sys, re, lxml, cgi, cgitb, unicodedata, locale
 from ConfigParser import *
 from amazonproduct import *
 
 cgitb.enable()
 
 
-def dosearch(isbnstring, page):
-    # api = API(os.getenv("AMAZON_ACCESS_KEY"), os.getenv("AMAZON_SECRET_KEY"), "us")
-    cfg = ConfigParser()
-    cfg.read("/etc/apache2/amazon.keys")
-    api = API(cfg.get("keys", "AMAZON_ACCESS_KEY"), cfg.get("keys", "AMAZON_SECRET_KEY"), "us")
-    
-    node = api.item_lookup(isbnstring, IdType="ISBN", SearchIndex="Books", MerchantId="All", Condition="All", ResponseGroup="Medium,Offers", OfferPage=page)
-    lxml.objectify.enable_recursive_str(True)
-    
+def dosearch(api, isbns, invalidisbns, page):
+    node = None
+    doagain = True
+    while (doagain):
+        doagain = False
+        isbnstring = ",".join(isbns)
+        try:
+            node = api.item_lookup(isbnstring, IdType="ISBN", SearchIndex="All", MerchantId="All", Condition="All", ResponseGroup="Medium,Offers", OfferPage=page)
+        except InvalidParameterValue, e:
+            #print "<p>InvalidParameterValue: %s" % str(e)
+            #print "<p>type(e.args): ", type(e.args)
+            #print "<p>dir(e): ", dir(e)
+            #print "<p>e.args: ", e.args
+            if e.args[0] == "ItemId":
+                invalidisbns[e.args[1]] = True
+                isbns.remove(e.args[1])
+                doagain = True
+            else:
+                raise e
     return node
 
 def safe_note(s):
@@ -26,7 +36,7 @@ def safe_note(s):
 def collectlowprices(bycond, isbn, node):
     try:
         for item in node.Items.Item:
-            if isbn == item.ItemAttributes.ISBN:
+            if isbn == item.ASIN:
                 for offer in item.Offers.Offer:
                     key = "%s %s" % (offer.OfferAttributes.Condition, offer.OfferAttributes.SubCondition)
                     if not bycond.has_key(key):
@@ -41,7 +51,7 @@ def collectlowprices(bycond, isbn, node):
                     except: 
                         pass
                     bycond[key].append("<span title='Merchant: %0.1f stars on %d ratings ConditionNote: %s'>" % (stars, ratings, condnote) + str(offer.OfferListing.Price.FormattedPrice) + "</span>")
-    except AttributeError as e:
+    except AttributeError, e:
         pass
 
 def outputsearch(node, node2):
@@ -53,7 +63,12 @@ def outputsearch(node, node2):
         print "<tr>"
         atr = item.ItemAttributes
         print "<td>"
-        print atr.Title, "<br>", atr.ISBN, "<br>by", atr.Author, ",", atr.Publisher, "sales rank:", item.SalesRank
+        author = ""
+        try:
+            author = atr.Author
+        except AttributeError, e:
+            author = atr.Artist
+        print atr.Title, "<br>ASIN: ", item.ASIN, "<br>by", author, ",", atr.Publisher, "sales rank:", locale.format("%d", int(item.SalesRank), True)
         print "</td>"
         
         offs = item.OfferSummary
@@ -73,8 +88,8 @@ def outputsearch(node, node2):
         print "</td>"
         
         bycond = dict()
-        collectlowprices(bycond, atr.ISBN, node)
-        collectlowprices(bycond, atr.ISBN, node2)
+        collectlowprices(bycond, item.ASIN, node)
+        collectlowprices(bycond, item.ASIN, node2)
 
         print "<td>"
         for key in bycond.keys():
@@ -85,6 +100,13 @@ def outputsearch(node, node2):
     print "</table>"
 
 
+# api = API(os.getenv("AMAZON_ACCESS_KEY"), os.getenv("AMAZON_SECRET_KEY"), "us")
+def make_apiobj():
+    cfg = ConfigParser()
+    cfg.read("/etc/apache2/amazon.keys")
+    return API(cfg.get("keys", "AMAZON_ACCESS_KEY"), cfg.get("keys", "AMAZON_SECRET_KEY"), "us")
+
+
 print "Content-Type: text/html\n\n"
 
 print "<html><head></head>\n<body>"
@@ -92,19 +114,24 @@ print "<html><head></head>\n<body>"
 isbnstring = ""
 form = cgi.FieldStorage()
 
+api = make_apiobj()
+lxml.objectify.enable_recursive_str(True)
+
+locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+
+isbns = list()
+invalidisbns = dict()
 if form.has_key("isbns"):
-    isbnstring = ', '.join(form["isbns"].value.split())
-if isbnstring == "":
-    #isbnstring = "0486247791, 0141304707"
-    pass
-else:
-    node = dosearch(isbnstring, 1)
-    node2 = dosearch(isbnstring, 2)
+    isbns = form["isbns"].value.split()
+    node = dosearch(api, isbns, invalidisbns, 1)
+    node2 = dosearch(api, isbns, invalidisbns, 2)
+    for i in invalidisbns.keys():
+        print "<p><b>INVALID ISBN: ", i, "</b>"
     outputsearch(node, node2)
+else:
+    pass
 
-print "<h3>Enter ISBNs 1 per line</h3>"
-
-print "<p>Sorry, books only right now, no DVD/CD/etc."
+print "<h3>Enter ISBNs (or UPC from CD/DVD/etc.) 1 per line</h3>"
 
 print "<form method='GET'>"
 print "<textarea width='80%' name=isbns rows=20></textarea>"
