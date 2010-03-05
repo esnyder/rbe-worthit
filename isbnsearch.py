4e1da8b2298a5a1932ae65dd3ac2cb58d7b26615
Emile Snyder <emile.snyder@gmail.com>
2010-03-04 16:42:36 -0800

    Fixed: * exception when there was no sales rank. * exception when there were only invalid isbns
    Enhanced:
    * process isbns one by one (doesn't seem to make a difference in speed) and output info after each
    * means that invalid isbns are in the right order and integrated into the result table
    * don't do the search for the second page of results if there isn't one
    * should be more robust against random exceptions now

--- isbnsearch.py
+++ isbnsearch.py
@@ -1,111 +1,127 @@
 #!/usr/bin/env python
 #
 
-import os, sys, re, lxml, cgi, cgitb, unicodedata, locale
+import os, sys, re, lxml, cgi, cgitb, unicodedata, locale, time
+import StringIO
 from ConfigParser import *
 from amazonproduct import *
 
 cgitb.enable()
 
 
-def dosearch(api, isbns, invalidisbns, page):
+def dosearch(api, isbn, page):
     node = None
-    doagain = True
-    while (doagain):
-        doagain = False
-        isbnstring = ",".join(isbns)
-        try:
-            node = api.item_lookup(isbnstring, IdType="ISBN", SearchIndex="All", MerchantId="All", Condition="All", ResponseGroup="Medium,Offers", OfferPage=page)
-        except InvalidParameterValue, e:
-            #print "<p>InvalidParameterValue: %s" % str(e)
-            #print "<p>type(e.args): ", type(e.args)
-            #print "<p>dir(e): ", dir(e)
-            #print "<p>e.args: ", e.args
-            if e.args[0] == "ItemId":
-                invalidisbns[e.args[1]] = True
-                isbns.remove(e.args[1])
-                doagain = True
-            else:
-                raise e
+    try:
+        node = api.item_lookup(isbn, IdType="ISBN", SearchIndex="All", MerchantId="All", Condition="All", ResponseGroup="Medium,Offers", OfferPage=page)
+    except InvalidParameterValue, e:
+        if e.args[0] == "ItemId":
+            pass
+        else:
+            raise e
     return node
 
 def safe_note(s):
     return unicodedata.normalize('NFKD', s).encode('ascii', 'ignore')
 
 # do it this way so we can get two or more pages of offers right off the bat
-def collectlowprices(bycond, isbn, node):
+def collectlowprices(bycond, item):
+    if item is None:
+        return
+
     try:
-        for item in node.Items.Item:
-            if isbn == item.ASIN:
-                for offer in item.Offers.Offer:
-                    key = "%s %s" % (offer.OfferAttributes.Condition, offer.OfferAttributes.SubCondition)
-                    if not bycond.has_key(key):
-                        bycond[key] = list()
-                    stars = 0
-                    ratings = 0
-                    condnote = "(none)"
-                    try: 
-                        stars = offer.Merchant.AverageFeedbackRating
-                        ratings = offer.Merchant.TotalFeedback
-                        condnote = safe_note(u'%s' % offer.OfferAttributes.ConditionNote)
-                    except: 
-                        pass
-                    bycond[key].append("<span title='Merchant: %0.1f stars on %d ratings ConditionNote: %s'>" % (stars, ratings, condnote) + str(offer.OfferListing.Price.FormattedPrice) + "</span>")
+        for offer in item.Offers.Offer:
+            key = "%s %s" % (offer.OfferAttributes.Condition, offer.OfferAttributes.SubCondition)
+            if not bycond.has_key(key):
+                bycond[key] = list()
+            stars = 0
+            ratings = 0
+            condnote = "(none)"
+            try: 
+                stars = offer.Merchant.AverageFeedbackRating
+                ratings = offer.Merchant.TotalFeedback
+                condnote = safe_note(u'%s' % offer.OfferAttributes.ConditionNote)
+            except: 
+                pass
+            bycond[key].append("<span title='Merchant: %0.1f stars on %d ratings ConditionNote: %s'>" % (stars, ratings, condnote) + str(offer.OfferListing.Price.FormattedPrice) + "</span>")
     except AttributeError, e:
         pass
 
 
-def firstof(lxmlnode, possibleattributes):
+def firstof(lxmlnode, possibleattributes, default="(none)"):
     for attr in possibleattributes:
         try:
             res = lxmlnode.__getattr__(attr)
             return res
         except AttributeError, e:
             pass
-    return "(none)"
+    return default
 
 
-def outputsearch(node, node2):
-    print "<table border='1'>"
-    
-    print "<tr><th>Item Details</th><th>Offer Summary</th><th>20 Lowest Priced Offers</th></tr>"
-    
-    for item in node.Items.Item:
-        print "<tr>"
+def formatitem(item, item2):
+    res = StringIO.StringIO()
+    try:
         atr = item.ItemAttributes
 
-        print "<td>"
+        print >>res, "<td>"
         author = firstof(atr, ["Author", "Artist", "Creator"])
         pub = firstof(atr, ["Publisher", "Label"])
-        print atr.Title, "<br>ASIN: ", item.ASIN, "<br>by", author, ",", pub, "sales rank:", locale.format("%d", int(item.SalesRank), True)
-        print "</td>"
-        
-        offs = item.OfferSummary
-        print "<td>"
-        try:
-            print "%d New from %s<br>" % (offs.TotalNew, offs.LowestNewPrice.FormattedPrice)
-        except:
-            print "0 New available<br>"
-        try:
-            print "%d Used from %s<br>" % (offs.TotalUsed, offs.LowestUsedPrice.FormattedPrice)
-        except:
-            print "0 Used available</br>"
-        try:
-            print "%d Collectible from %s<br>" % (offs.TotalCollectible, offs.LowestCollectiblePrice.FormattedPrice)
-        except:
-            print "0 Collectible available</br>"
-        print "</td>"
-        
-        bycond = dict()
-        collectlowprices(bycond, item.ASIN, node)
-        collectlowprices(bycond, item.ASIN, node2)
+        sr = firstof(item, ["SalesRank"], 0)
+        print >>res, atr.Title, "<br>ASIN: ", item.ASIN, "<br>by", author, ",", pub, "sales rank:", locale.format("%d", int(sr), True)
+        print >> res, "</td>"
 
-        print "<td>"
+        offs = item.OfferSummary
+        print >>res, "<td>"
+        try:
+            print >>res, "%d New from %s<br>" % (offs.TotalNew, offs.LowestNewPrice.FormattedPrice)
+        except:
+            print >>res, "0 New available<br>"
+        try:
+            print >>res, "%d Used from %s<br>" % (offs.TotalUsed, offs.LowestUsedPrice.FormattedPrice)
+        except:
+            print >>res, "0 Used available</br>"
+        try:
+            print >>res, "%d Collectible from %s<br>" % (offs.TotalCollectible, offs.LowestCollectiblePrice.FormattedPrice)
+        except:
+            print >>res, "0 Collectible available</br>"
+        print >>res, "</td>"
+
+        bycond = dict()
+        collectlowprices(bycond, item)
+        collectlowprices(bycond, item2)
+
+        print >>res, "<td>"
         for key in bycond.keys():
-            print "%s: %s" % (key, ' '.join(bycond[key]))
-            print "<br>"
-        print "</td>"
-        print "</tr>"
+            print >>res, "%s: %s" % (key, ' '.join(bycond[key]))
+            print >>res, "<br>"
+        print >>res, "</td>"
+        
+        print >>res, "</tr>"
+
+        # debugging, dump the item into the table too
+        #print >>res, "<tr><td colspan=3>", str(item).replace("\n", "<br>"), "</td></tr>"
+    except:
+        res.truncate(0)
+        print >>res, "<tr><td colspan=3>Unknown exception: ", str(sys.exc_type), str(sys.exc_value), str(sys.exc_traceback), "</td></tr>"
+    return res.getvalue()
+
+
+def process_isbns(isbns):
+    print "<table border='1'>"
+    print "<tr><th>Item Details</th><th>Offer Summary</th><th>20 Lowest Priced Offers</th></tr>"
+
+    for isbn in isbns:
+        node = dosearch(api, isbn, 1)
+
+        if node is None:
+            print "<tr><td colspan=3><b>INVALID ISBN: ", isbn, "</b></td></tr>"
+        else:
+            item2 = None
+            if node.Items.Item.Offers.TotalOffers > 10:
+                node2 = dosearch(api, isbn, 2)
+                if node2 is not None: item2 = node2.Items.Item
+            print formatitem(node.Items.Item, item2)
+
+        sys.stdout.flush()
     print "</table>"
 
 
@@ -117,7 +133,6 @@ def make_apiobj():
 
 
 print "Content-Type: text/html\n\n"
-
 print "<html><head></head>\n<body>"
 
 isbnstring = ""
@@ -132,11 +147,7 @@ isbns = list()
 invalidisbns = dict()
 if form.has_key("isbns"):
     isbns = form["isbns"].value.split()
-    node = dosearch(api, isbns, invalidisbns, 1)
-    node2 = dosearch(api, isbns, invalidisbns, 2)
-    for i in invalidisbns.keys():
-        print "<p><b>INVALID ISBN: ", i, "</b>"
-    outputsearch(node, node2)
+    process_isbns(isbns)
 else:
     pass
 
